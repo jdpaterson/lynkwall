e8ac38cd5f73bea0ceae471408e206eab8d367d1
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-06-28 22:52:17 +0000

    change way we seed the users table

--- routes/resources.js
+++ routes/resources.js
@@ -15,14 +15,18 @@ module.exports = (knex) => {
     });
   });
 
-  router.get("/new", (req, res) => {
-    knex
-      .select("*")
-      .from("resources")
-      .then((results) => {  
-        //res.json(results);
-        res.render("index", {resources: results})
-    });
+  router.post("/new", (req, res) => {
+     
+    knex('resources')
+      .insert({
+        URL: req.body.URL, 
+        title: req.body.title,
+        description: req.body.description,
+        imageURL: req.body.imageURL,
+        creator_id: req.body.creator_id
+      })
+      res.redirect('/');
+       
   });
 
   return router;
