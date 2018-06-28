2d03bb59b41d0314e654dbc7a397d0d05dd203ea
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-06-28 00:47:03 +0000

    create categories table

---
+++ db/migrations/20180628004309_categories.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.createTable('categories', function (table) {
+      table.increments('category_id');
+      table.string('category');
+      table.date('created_on');
+      table.date('updated_on');
+      
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('categories')
+  ])
+};
