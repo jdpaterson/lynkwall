0535e45495bf2d88f5061f3ca4294a2c68027727
Jeremy Paterson <jaydpaterson@gmail.com>
2018-06-28 18:41:11 +0000

    Fixing bugs in seed and migration files

--- db/seeds/04-likes.js
+++ db/seeds/04-likes.js
@@ -2,11 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('likes').del()
     .then(function () {
       return Promise.all([
-        knex('likes').insert({created_on: '2018-06-28', updated_on: null, resource_id: 4, user_id: 1}),
-        knex('likes').insert({created_on: '2018-06-27', updated_on: null, resource_id: 5, user_id: 2}),
-        knex('likes').insert({created_on: '2018-06-26', updated_on: null, resource_id: 6, user_id: 3})
+        knex('likes').insert({created_on: '2018-06-28', updated_on: null, resource_id: 1, user_id: 1}),
+        knex('likes').insert({created_on: '2018-06-27', updated_on: null, resource_id: 2, user_id: 2}),
+        knex('likes').insert({created_on: '2018-06-26', updated_on: null, resource_id: 3, user_id: 3})
       ]);
     });
 };
-
-
--- db/seeds/05-categories_resources.js
+++ db/seeds/05-categories_resources.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('categories_resources').del()
     .then(function () {
       return Promise.all([
-        knex('categories_resources').insert({category_id: 1, resource_id: 4}),
-        knex('categories_resources').insert({category_id: 2, resource_id: 5}),
-        knex('categories_resources').insert({category_id: 3, resource_id: 6})
+        knex('categories_resources').insert({category_id: 1, resource_id: 1}),
+        knex('categories_resources').insert({category_id: 2, resource_id: 2}),
+        knex('categories_resources').insert({category_id: 3, resource_id: 3})
       ]);
     });
 };
