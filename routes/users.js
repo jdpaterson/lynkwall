b4d03432bddbbded292106af9b038c9bba00227e
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-06-28 20:24:29 +0000

    added route for user likes

--- routes/users.js
+++ routes/users.js
@@ -15,17 +15,17 @@ module.exports = (knex) => {
   });
 
   router.get ("/:userid/resources", (req, res) => {   // get all resources created by a user
-    const resources_created = knex
-    .select("*")
-    .from("users")
-    .where("creator", userid);
+    knex
+      .select("*")
+      .from("users")
+      .where("creator", userid);
   })
   
   router.get ("/:userid/likes", (req, res) => {   // get all likes for a user
-    const resources_liked = knex
-    .select("*")
-    .from ("likes")
-    .where("user_id", userid);
+    knex
+      .select("*")
+      .from ("likes")
+      .where("user_id", userid);
   })
 
   return router;
