3fcd983546e69d6ec6cebaad275f5ea789397d22
Jeremy Paterson <jaydpaterson@gmail.com>
2018-06-28 19:22:41 +0000

    Adding ejs partials

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,9 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
+    console.log(users);
     for(user of users) {
+
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,6 @@ module.exports = (knex) => {
         res.json(results);
     });
   });
-
+  
   return router;
 }
