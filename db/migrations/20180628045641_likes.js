bf60b13f9aad05ebc880de5088f044c99be5b0f3
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-06-28 05:07:04 +0000

    create like table

---
+++ db/migrations/20180628045641_likes.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.createTable('likes', function (table) {
+      table.increments('like_id');
+      table.date('created_on');
+      table.date('updated_on'); 
+      table.integer('resource_id').unsigned().notNullable();
+      table.integer('user_id').unsigned().notNullable();
+  
+      table.foreign('resource_id').references('resource_id').inTable('resources');
+      table.foreign('user_id').references('id').inTable('users');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('likes')
+  ])
+};
