35dd6713cc533d3a8b7b9b37ad24630cd2976eda
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-06-29 22:36:40 +0000

    add likes.js for the like route

---
+++ routes/likes.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/:userid", (req, res) => {
+    const userid = req.params.userid;
+    knex
+      .select("*")
+      .from("likes")
+      .where("user_id", userid)
+      .then((results) => {  
+        res.json(results);
+        //res.render("index", {resources: results})
+
+      })
+    })
+      
+  router.get("/:resourceid", (req, res) => {
+    const resource_id = req.params.resourceid;
+    knex
+      .select("*")
+      .from("likes")
+      .where("resource_id", resource_id )
+      .then((results) => {
+        res.json(results);
+    });
+  });
+
+  router.post("/:resourceid/new", (req, res) => {
+    const resource_id = req.params.resourceid;
+    knex('likes')
+      .insert({
+        created_on: req.body.created_on,
+        updated_on: null,
+        resource_id: resource_id,
+        user_id: 1 //hardcoded for now
+      }).then((result)=>{
+      res.redirect('/');
+    });
+ });
+
+  return router;
+}
