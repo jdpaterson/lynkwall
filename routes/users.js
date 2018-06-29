d9713d4943fcc1c95d580eafa66967e3e6f2f4af
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-06-29 00:01:57 +0000

    created routes for GET/users and POST for a new user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
 
@@ -14,7 +14,7 @@ module.exports = (knex) => {
       });
   });
 
-  router.get ("/:userid/resources", (req, res) => {   // get all resources created by a user
+  router.get("/:userid/resources", (req, res) => {   // get all resources created by a user
     const userid = req.params.userid;
     knex
       .select("*")
@@ -22,23 +22,31 @@ module.exports = (knex) => {
       .where("creator_id", userid)
       .then((results) => {
         res.json(results);
-      }).catch((err)=> {
+      }).catch((err) => {
         console.log('AAAAAAAHHH', err)
       })
   })
 
-  router.get ("/:userid/likes", (req, res) => {   // get all likes for a user
+  router.get("/:userid/likes", (req, res) => {   // get all likes for a user
     const userid = req.params.userid;
     knex
       .select("*")
-      .from ("likes")
+      .from("likes")
       .where("user_id", userid)
       .then((results) => {
         res.json(results);
       }).catch((err) => {
-        console.log ('aaaaaarrgghh', err)
+        console.log('aaaaaarrgghh', err)
       })
   })
+  router.post("/new", (req, res) => {   // create new resource 
+    const name = req.body.name;
+    knex ('resources')
+      .insert({name: name})
+})
+res.redirect('/');
+
+
 
   return router;
 }
