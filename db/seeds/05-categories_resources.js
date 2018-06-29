3cca2440d36147ce9baa626aeadea5629cbeed43
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-06-29 20:11:27 +0000

    seed database with more data

--- db/seeds/05-categories_resources.js
+++ db/seeds/05-categories_resources.js
@@ -4,7 +4,9 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('categories_resources').insert({category_id: 1, resource_id: 1}),
         knex('categories_resources').insert({category_id: 2, resource_id: 2}),
-        knex('categories_resources').insert({category_id: 3, resource_id: 3})
+        knex('categories_resources').insert({category_id: 3, resource_id: 3}),
+        knex('categories_resources').insert({category_id: 1, resource_id: 4}),
+        knex('categories_resources').insert({category_id: 1, resource_id: 5}),
       ]);
     });
 };
