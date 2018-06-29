d5422a0e52d0f8ca5e5c6091d66db51e5dbe4294
Jeremy Paterson <jaydpaterson@gmail.com>
2018-06-29 18:20:25 +0000

    Like button functionality and server route

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
-  $('.like-button').children('.fas').hide();
-  $('.like-button').on('click', function(ev){
-    let resourceId = ev.delegateTarget.dataset.res_id;
-    /*$.ajax(`/${res_id}/likes`, {
-          method: 'POST',
-      }).done(function(){
-        $('#new-tweet').hide(200);
-        getTweets();
-      });*/
-    $(ev.delegateTarget).children('.far').hide();
-    $(ev.delegateTarget).children('.fas').fadeIn();
+$(document).ready(function() {
+  $('.like-button').on('click', function(ev) {
+      //console.log(ev.delegateTarget.dataset.res_id);
+      let data = {
+        resourceid: ev.delegateTarget.dataset.res_id,
+        userid: 1
+      };
+
+      $.post(`/resources/${data.resourceid}/likes`, data);
+
+      $(ev.delegateTarget).children('i').toggleClass('far');
+      $(ev.delegateTarget).children('i').toggleClass('fas');
+
   });
 })
