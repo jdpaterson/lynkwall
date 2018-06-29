6e691dc1b0728f8d92fd6f0fdd6e58267ad6cabd
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-06-29 02:38:42 +0000

    add post new resource and get all like for a resource

--- routes/resources.js
+++ routes/resources.js
@@ -21,11 +21,22 @@ module.exports = (knex) => {
   });
 
   router.get("/:resourceid/comments", (req, res) => {
-    console.log(req.baseUrl)
     const resource_id = req.params.resourceid;
     knex
       .select("*")
       .from("comments")
+      .where("resource_id", resource_id )
+      .then((results) => {  
+        res.json(results);
+       // res.render("index", {resources: results}) -what is the page for comment
+    });
+  });
+
+  router.get("/:resourceid/likes", (req, res) => {
+    const resource_id = req.params.resourceid; 
+    knex
+      .count('like_id')
+      .from("likes")
       .where("resource_id", resource_id )
       .then((results) => {  
         res.json(results);
@@ -48,10 +59,8 @@ module.exports = (knex) => {
   });
 
   router.post("/:resourceid/comments", (req, res) => {
-     console.log('inside post ');
-     console.log('rg body is', req.body);
 
-     const {conment_text, created_on, updated_on, resource_id, user_id} = req.body;
+    const {conment_text, created_on, updated_on, resource_id, user_id} = req.body;
     knex('comment')
     .insert({
       comment_tex, 
@@ -61,8 +70,6 @@ module.exports = (knex) => {
       user_id
     })
     .then();
-    
-    
     return res.redirect('/');
        
   });
