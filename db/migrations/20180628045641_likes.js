f507584b8f59b44ef5759ed874719443c8e01e9b
jdpaterson <jaydpaterson@gmail.com>
2018-08-12 16:53:20 -0700

    Changed the migration files so that each table has an appropriately name id field

--- db/migrations/20180628045641_likes.js
+++ db/migrations/20180628045641_likes.js
@@ -1,13 +1,13 @@
-exports.up = function(knex, Promise) {  
+exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('likes', function (table) {
-      table.increments('like_id');
+      table.increments();
       table.date('created_on');
-      table.date('updated_on'); 
+      table.date('updated_on');
       table.integer('resource_id').unsigned().notNullable();
       table.integer('user_id').unsigned().notNullable();
-  
-      table.foreign('resource_id').references('resource_id').inTable('resources');
+
+      table.foreign('resource_id').references('id').inTable('resources');
       table.foreign('user_id').references('id').inTable('users');
     })
   ])
--- db/migrations/20180628053616_rating.js
+++ db/migrations/20180628053616_rating.js
@@ -1,14 +1,14 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('ratings', function (table) {
-      table.increments('rating_id');
+      table.increments();
       table.integer('rating');
       table.date('created_on');
       table.date('updated_on');
       table.integer('resource_id').unsigned().notNullable();
       table.integer('user_id').unsigned().notNullable();
 
-      table.foreign('resource_id').references('resource_id').inTable('resources');
+      table.foreign('resource_id').references('id').inTable('resources');
       table.foreign('user_id').references('id').inTable('users');
     })
   ])
